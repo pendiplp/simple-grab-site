fd8a71a944056a7fea0f4464bdd44e3172db53fd
Fendy Kharisma <pendiplp@gmail.com>
2016-12-13 13:40:19 +0700

    add /restart

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const express = require('express')
 const app = express();
 
 app
+	.get('/restart', () => { process.exit() })
 	.get('/*', (req, res) => {
 		if (req.url == '/favicon.ico') return;
 
